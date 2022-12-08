7b01f7cc05eaa403e9051abcf558bb56e688777b
Cam-Black <cameronablack1998@gmail.com>
2022-12-08 09:43:09 +0000

    Created Person class as my template class to write to file from.

---
+++ src/main/java/com/virginmoney/uk/template/Person.java
@@ -0,0 +1,53 @@
+package com.virginmoney.uk.template;
+
+import java.time.LocalDate;
+
+public class Person {
+	
+	private String firstName;
+	private String lastName;
+	private LocalDate dob;
+	
+	public Person() {
+	}
+	
+	public Person(String firstName, String lastName, LocalDate age) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dob = age;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public LocalDate getDob() {
+		return dob;
+	}
+	
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+	
+	@Override
+	public String toString() {
+		return "Person [" +
+				"first name: " + this.getFirstName() +
+				", last name: " + this.getLastName() +
+				", age=" + this.getDob() +
+				"]";
+	}
+}
