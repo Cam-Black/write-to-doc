c0b7025506c438f9e2739c8e2ad34695e67eaf44
Cam-Black <cameronablack1998@gmail.com>
2022-12-08 10:17:45 +0000

    Created class to write to a file. Created a LOGGER field to use log4j2 for logging.

---
+++ src/main/java/com/virginmoney/uk/writer/TemplateToFile.java
@@ -0,0 +1,9 @@
+package com.virginmoney.uk.writer;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class TemplateToFile {
+	private  final Logger LOGGER = LogManager.getLogger();
+	
+}
