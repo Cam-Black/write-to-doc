16074a1a3b7ad26b68882e8a39b1390d762b6260
cam-black <cameronablack1998@gmail.com>
2022-12-13 10:41:06 +0000

    Changed populateAndSaveDoc to return an int to count number of paragraphs so can test method. Added forEach method to write each object on it's own line in doc.

--- src/main/java/com/virginmoney/uk/writer/ObjectToFile.java
+++ src/main/java/com/virginmoney/uk/writer/ObjectToFile.java
@@ -12,38 +12,44 @@ import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class ObjectToFile<T> {
-	private final Logger LOGGER = LogManager.getLogger();
-	private final Path path;
-	
-	public ObjectToFile() {
-		super();
-		path = Path.of("output.docx");
-	}
-	
-	public void populateAndSaveDoc(List<T> objects) {
-		try (XWPFDocument doc = new XWPFDocument(); OutputStream out = Files.newOutputStream(path)) {
-			LOGGER.info("Creating document...");
-			XWPFParagraph para = doc.createParagraph();
-			XWPFRun run = para.createRun();
-			run.setText(convertObjToString(objects));
-			LOGGER.info("Writing to document...");
-			doc.write(out);
-			LOGGER.info("Document created.");
-		} catch (FileNotFoundException fnfe) {
-			LOGGER.error("File not found!");
-		} catch (IOException ioe) {
-			LOGGER.error(ioe);
-		}
-	}
-	
-	public String convertObjToString(List<T> obj) {
-		StringBuilder sb = new StringBuilder();
-		obj.forEach(el -> {
-			sb.append(el.toString());
-			sb.append("\n");
-		});
-		return sb.toString();
-	}
+    private final Logger LOGGER = LogManager.getLogger();
+    private final Path path;
+
+    public ObjectToFile() {
+        super();
+        path = Path.of("output.docx");
+    }
+
+    public int populateAndSaveDoc(List<T> objects) {
+        AtomicInteger paraCount = new AtomicInteger();
+        try (XWPFDocument doc = new XWPFDocument(); OutputStream out = Files.newOutputStream(path)) {
+            LOGGER.info("Document Created");
+            objects.forEach(el -> {
+                LOGGER.info("Writing to document...");
+                XWPFParagraph para = doc.createParagraph();
+                XWPFRun run = para.createRun();
+                run.setText(convertObjToString(el));
+                paraCount.getAndIncrement();
+            });
+            doc.write(out);
+            LOGGER.info("Document saved.");
+            return paraCount.get();
+        } catch (FileNotFoundException fnfe) {
+            LOGGER.error("File not found!");
+        } catch (IOException ioe) {
+            LOGGER.error(ioe);
+        }
+        return 0;
+    }
+
+    public String convertObjToString(T obj) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(obj.toString());
+        sb.append("\n");
+        return sb.toString();
+    }
 }
