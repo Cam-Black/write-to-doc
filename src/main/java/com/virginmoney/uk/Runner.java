67f39b39df9d965fb52876f194811bbf99326c03
cam-black <cameronablack1998@gmail.com>
2022-12-13 16:45:24 +0000

    Tested new method in Runner to confirm expected output.

--- src/main/java/com/virginmoney/uk/Runner.java
+++ src/main/java/com/virginmoney/uk/Runner.java
@@ -3,18 +3,20 @@ package com.virginmoney.uk;
 import com.virginmoney.uk.template.Person;
 import com.virginmoney.uk.writer.PersonToFile;
 
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Runner {
-	public static void main(String[] args) {
-		PersonToFile toFile = new PersonToFile();
-		List<Person> people = new ArrayList<>();
-		Person one = new Person("John", "Doe", LocalDate.of(2000, 5, 11));
-		Person two = new Person("Jane", "Doe", LocalDate.of(2020, 7, 16));
-		people.add(one);
-		people.add(two);
-		toFile.populateAndSaveDoc(people);
-	}
+    public static void main(String[] args) {
+        PersonToFile toFile = new PersonToFile(Paths.get("input.docx"), Paths.get("output.docx"));
+//		List<Person> people = new ArrayList<>();
+        Person one = new Person("John", "Doe", LocalDate.of(2000, 5, 11));
+//		Person two = new Person("Jane", "Doe", LocalDate.of(2020, 7, 16));
+//		people.add(one);
+//		people.add(two);
+//		toFile.populateAndSaveDoc(people);
+        toFile.replacePlaceholdersInFile(one);
+    }
 }
