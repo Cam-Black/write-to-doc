88203fc1cea1f9f631625067ffc082f0cabfb40f
cam-black <cameronablack1998@gmail.com>
2022-12-13 09:38:19 +0000

    Implemented Lombok annotation and removed unneeded code.

--- src/main/java/com/virginmoney/uk/template/Person.java
+++ src/main/java/com/virginmoney/uk/template/Person.java
@@ -1,29 +1,17 @@
 package com.virginmoney.uk.template;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import java.time.LocalDate;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode
 @Getter
 @Setter
 @ToString
 public class Person {
-	
-	private String firstName;
-	private String lastName;
-	private LocalDate dob;
-	
-	public Person() {
-	}
-	
-	public Person(String firstName, String lastName, LocalDate dob) {
-		super();
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.dob = dob;
-	}
+    private String firstName;
+    private String lastName;
+    private LocalDate dob;
 }
