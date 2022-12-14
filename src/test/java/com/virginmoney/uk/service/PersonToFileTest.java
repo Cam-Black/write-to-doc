cb31963cabcca99a0814b5b91643b0e30a8da846
cam-black <cameronablack1998@gmail.com>
2022-12-14 16:50:51 +0000

    Implemented passing test for replacePlaceholdersInFile method.

--- src/test/java/com/virginmoney/uk/service/PersonToFileTest.java
+++ src/test/java/com/virginmoney/uk/service/PersonToFileTest.java
@@ -1,4 +1,26 @@
 package com.virginmoney.uk.service;
 
+import com.virginmoney.uk.entity.Person;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class PersonToFileTest {
+    @Mock
+    PersonToFile toFile;
+
+    @Test
+    public void helloJohnDoe() {
+        Person person = new Person();
+        String text = "Hello John Doe. Your date of birth is 2000-05-23.";
+
+        Mockito.when(toFile.replacePlaceholdersInFile(person)).thenReturn(text);
+
+        Assertions.assertEquals(text, toFile.replacePlaceholdersInFile(person));
+        Mockito.verify(toFile, Mockito.times(1)).replacePlaceholdersInFile(person);
+    }
 }
