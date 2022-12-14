b726747c59ac040ad81453210bb8b3a8166e49dd
cam-black <cameronablack1998@gmail.com>
2022-12-14 14:41:22 +0000

    Refactored method to take a String as parameter and then create a document from that String. Return an int to count number of lines for testing.

---
+++ src/main/java/com/virginmoney/uk/utils/FileWriter.java
@@ -0,0 +1,42 @@
+package com.virginmoney.uk.utils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileWriter {
+    private static final Logger LOGGER = LogManager.getLogger();
+    private static final Path OUT = Paths.get("output.docx");
+
+    public static int populateDoc(String text) {
+        int count = 0;
+        try (XWPFDocument doc = new XWPFDocument();
+             OutputStream out = Files.newOutputStream(OUT)) {
+            LOGGER.info("Document Created.");
+            String[] lines = text.split("\n");
+            for (String p : lines) {
+                XWPFParagraph para = doc.createParagraph();
+                XWPFRun run = para.createRun();
+                run.setText(p);
+                count++;
+            }
+            LOGGER.info("Document saved.");
+            doc.write(out);
+            return count;
+        } catch (FileNotFoundException fnfe) {
+            LOGGER.error("File not found!");
+        } catch (IOException ioe) {
+            LOGGER.error(ioe);
+        }
+        return count;
+    }
+}
