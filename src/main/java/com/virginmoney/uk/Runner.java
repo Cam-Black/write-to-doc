51e426d8e8190a6d5a110f5c1b18b84fa9989c38
cam-black <cameronablack1998@gmail.com>
2022-12-14 14:42:52 +0000

    Refactored Runner to test output of refactored methods in PersonToFile class.

--- src/main/java/com/virginmoney/uk/Runner.java
+++ src/main/java/com/virginmoney/uk/Runner.java
@@ -1,22 +1,14 @@
 package com.virginmoney.uk;
 
-import com.virginmoney.uk.template.Person;
-import com.virginmoney.uk.writer.PersonToFile;
+import com.virginmoney.uk.entity.Person;
+import com.virginmoney.uk.service.PersonToFile;
 
-import java.nio.file.Paths;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Runner {
     public static void main(String[] args) {
-        PersonToFile toFile = new PersonToFile(Paths.get("input.docx"), Paths.get("output.docx"));
-//		List<Person> people = new ArrayList<>();
-        Person one = new Person("John", "Doe", LocalDate.of(2000, 5, 11));
-//		Person two = new Person("Jane", "Doe", LocalDate.of(2020, 7, 16));
-//		people.add(one);
-//		people.add(two);
-//		toFile.populateAndSaveDoc(people);
-        toFile.replacePlaceholdersInFile(one);
+        PersonToFile tf = new PersonToFile("input.docx");
+        Person p = new Person("John", "Doe", LocalDate.of(2000, 5, 23));
+        tf.replacePlaceholdersInFile(p);
     }
 }
