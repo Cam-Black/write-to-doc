1941fb7f1bf08ec3549c519ee3b6168c850175f3
cam-black <cameronablack1998@gmail.com>
2022-12-15 15:23:50 +0000

    Implemented tests to test readDoc method and tests to check exceptions caught in method.

--- src/test/java/com/virginmoney/uk/utils/FileReaderTest.java
+++ src/test/java/com/virginmoney/uk/utils/FileReaderTest.java
@@ -1,5 +1,28 @@
 package com.virginmoney.uk.utils;
 
-public class FileReaderTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+public class FileReaderTest {
+    @Test
+    void testReadDocReturnsTextFromTestFile() {
+        FileReader fileReader = new FileReader("src/test/resources/inputTest.docx");
+        String expected = "Hello, this is a test document.\n";
+
+        Assertions.assertEquals(expected, fileReader.readDoc());
+    }
+
+    @Test
+    void testNoSuchFileException() {
+        FileReader fileReader = new FileReader("null.docx");
+
+        Assertions.assertNull(fileReader.readDoc());
+    }
+
+    @Test
+    void testEmptyFileException() {
+        FileReader fileReader = new FileReader("src/test/resources/empty_file_exception_test.txt");
+
+        Assertions.assertNull(fileReader.readDoc());
+    }
 }
