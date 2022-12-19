c8cc33b6fa79842410a43c5528304116fd29d30f
cam-black <cameronablack1998@gmail.com>
2022-12-19 16:18:39 +0000

    Implemented Placeholders interface for Person Object, setting key-value pairs for Person fields.

---
+++ src/main/java/com/virginmoney/uk/utils/PersonPlaceholders.java
@@ -0,0 +1,17 @@
+package com.virginmoney.uk.utils;
+
+import com.virginmoney.uk.entity.Person;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PersonPlaceholders implements Placeholders<Person, String, String> {
+    @Override
+    public Map<String, String> getPlaceholders(Person person) {
+        Map<String, String> valueMap = new HashMap<>();
+        valueMap.put("first_name", person.getFirstName());
+        valueMap.put("last_name", person.getLastName());
+        valueMap.put("dob", person.getDob().toString());
+        return valueMap;
+    }
+}
