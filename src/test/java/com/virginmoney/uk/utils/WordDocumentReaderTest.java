b7405b3e9a0a5c0f581f89d28b1dd22523f071f2
cam-black <cameronablack1998@gmail.com>
2022-12-20 14:33:14 +0000

    changed Assertions to use static import and implemented tests for readDoc method, ensuring correct return and testing NoSuchFileException and EmptyFileException catch blocks.

--- src/test/java/com/virginmoney/uk/utils/WordDocumentReaderTest.java
+++ src/test/java/com/virginmoney/uk/utils/WordDocumentReaderTest.java
@@ -1,28 +1,44 @@
 package com.virginmoney.uk.utils;
 
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 public class WordDocumentReaderTest {
+    private WordDocumentMaker maker;
+
+    @BeforeEach
+    void setup() {
+        maker = new WordDocumentMaker();
+    }
+
     @Test
     void testReadDocReturnsTextFromTestFile() {
-        WordDocumentReader wordDocumentReader = new WordDocumentReader("src/test/resources/inputTest.docx");
-        String expected = "Hello, this is a test document.\n";
+        WordDocumentReader reader = new WordDocumentReader("src/test/resources/inputTest.docx", maker);
+        String expected = "Hello, this is a test document.";
 
-        Assertions.assertEquals(expected, wordDocumentReader.readDoc());
+        List<XWPFParagraph> lines = reader.readDoc();
+        StringBuilder textToString = new StringBuilder();
+        lines.forEach(el -> textToString.append(el.getText()));
+        Assertions.assertEquals(expected, textToString.toString());
     }
 
     @Test
     void testNoSuchFileException() {
-        WordDocumentReader wordDocumentReader = new WordDocumentReader("null.docx");
+        WordDocumentReader wordDocumentReader = new WordDocumentReader("null.docx", maker);
 
-        Assertions.assertNull(wordDocumentReader.readDoc());
+        assertNull(wordDocumentReader.readDoc());
     }
 
     @Test
     void testEmptyFileException() {
-        WordDocumentReader wordDocumentReader = new WordDocumentReader("src/test/resources/empty_file_exception_test.txt");
+        WordDocumentReader wordDocumentReader = new WordDocumentReader("src/test/resources/empty_file_exception_test.txt", maker);
 
-        Assertions.assertNull(wordDocumentReader.readDoc());
+        assertNull(wordDocumentReader.readDoc());
     }
 }
