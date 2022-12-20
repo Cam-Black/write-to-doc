28d6ed1cf0889d13c45f458c8a29663e5cdd4187
cam-black <cameronablack1998@gmail.com>
2022-12-20 14:36:55 +0000

    Refactored replacePlaceholdersInText method to utilise helper classes and split functionality to improve ability to test class and methods.

--- src/main/java/com/virginmoney/uk/service/PersonToFile.java
+++ src/main/java/com/virginmoney/uk/service/PersonToFile.java
@@ -3,6 +3,7 @@ package com.virginmoney.uk.service;
 import com.virginmoney.uk.entity.Person;
 import com.virginmoney.uk.utils.PersonPlaceholders;
 import com.virginmoney.uk.utils.WordDocumentReader;
+import com.virginmoney.uk.utils.WordDocumentWriter;
 import org.apache.commons.text.StringSubstitutor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,24 +13,32 @@ import java.util.Map;
 public class PersonToFile {
     private final Logger LOGGER = LogManager.getLogger();
     private final WordDocumentReader READER;
+    public final WordDocumentWriter WRITER;
 
     public PersonToFile() {
         super();
         READER = new WordDocumentReader();
+        WRITER = new WordDocumentWriter();
     }
 
-    public PersonToFile(WordDocumentReader reader) {
+    public PersonToFile(WordDocumentReader reader, WordDocumentWriter writer) {
         super();
         READER = reader;
+        WRITER = writer;
     }
 
     public StringBuilder replacePlaceholdersInText(Person person) {
         PersonPlaceholders placeholders = new PersonPlaceholders();
-        StringBuilder text = READER.readDoc();
+        StringBuilder text = new StringBuilder();
+        READER.readDoc().forEach(el -> {
+            text.append(el.getText());
+            text.append("\n");
+        });
         Map<String, String> toReplace = placeholders.getPlaceholders(person);
         StringSubstitutor sub = new StringSubstitutor(toReplace);
         text.replace(0, text.length(), sub.replace(text));
         LOGGER.info(text);
+        WRITER.populateDoc(text);
         return text;
     }
 }
