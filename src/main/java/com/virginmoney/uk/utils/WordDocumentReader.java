b1db59ff4d6c316bc7e99848e96749ddc3dbfac1
cam-black <cameronablack1998@gmail.com>
2022-12-20 14:31:51 +0000

    Changed return of method to return a list of XWPFParagraph so can return text even if an IOException occurs during execution.

--- src/main/java/com/virginmoney/uk/utils/WordDocumentReader.java
+++ src/main/java/com/virginmoney/uk/utils/WordDocumentReader.java
@@ -7,10 +7,10 @@ import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class WordDocumentReader {
@@ -30,30 +30,27 @@ public class WordDocumentReader {
         MAKER = maker;
     }
 
-    public StringBuilder readDoc() {
-        StringBuilder sb = new StringBuilder();
+    public List<XWPFParagraph> readDoc() {
+        List<XWPFParagraph> lines = new ArrayList<>();
         try (XWPFDocument doc = MAKER.newDocument(IN)) {
-            List<XWPFParagraph> lines = doc.getParagraphs();
+            lines = doc.getParagraphs();
             LOGGER.info("Reading file...");
-            lines.forEach(el -> {
-                sb.append(el.getText());
-                sb.append("\n");
-            });
             LOGGER.info("===========");
             LOGGER.info("FILE START");
-            LOGGER.info("===========\n");
-            LOGGER.info(sb);
+            LOGGER.info("===========");
+            lines.forEach(el -> LOGGER.info(el.getText()));
             LOGGER.info("===========");
             LOGGER.info("FILE END");
             LOGGER.info("===========\n");
-            return sb;
+            return lines;
         } catch (NoSuchFileException nfe) {
             LOGGER.error("File not found! Please check file path and name is correct!");
         } catch (EmptyFileException efe) {
             LOGGER.error("The file is empty, no content to read");
         } catch (IOException ioe) {
             LOGGER.error(ioe);
+            return lines;
         }
-        return sb;
+        return null;
     }
 }
