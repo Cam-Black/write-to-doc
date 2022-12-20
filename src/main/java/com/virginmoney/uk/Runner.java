a30eebfda263d92869e99262b520491dca579f39
cam-black <cameronablack1998@gmail.com>
2022-12-20 14:38:26 +0000

    Commented out code used for testing real methods work as intended.

--- src/main/java/com/virginmoney/uk/Runner.java
+++ src/main/java/com/virginmoney/uk/Runner.java
@@ -6,8 +6,8 @@ import com.virginmoney.uk.service.PersonToFile;
 import java.time.LocalDate;
 
 public class Runner {
-    public static void main(String[] args) {
-        PersonToFile toFile = new PersonToFile();
-        toFile.replacePlaceholdersInText(new Person("John", "Doe", LocalDate.of(2000, 2, 12)));
-    }
+//    public static void main(String[] args) {
+//        PersonToFile toFile = new PersonToFile();
+//        toFile.replacePlaceholdersInText(new Person("John", "Doe", LocalDate.of(2000, 2, 12)));
+//    }
 }
