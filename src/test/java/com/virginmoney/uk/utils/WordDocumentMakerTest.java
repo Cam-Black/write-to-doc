6e580e5d6bead71cc685d3f34f1c7d4c42c9b942
cam-black <cameronablack1998@gmail.com>
2022-12-20 14:41:13 +0000

    Implemented passing tests for WriteDocument method and associated IOException catch block.

---
+++ src/test/java/com/virginmoney/uk/utils/WordDocumentMakerTest.java
@@ -0,0 +1,44 @@
+package com.virginmoney.uk.utils;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class WordDocumentMakerTest {
+
+    private final WordDocumentMaker maker = new WordDocumentMaker();
+    private XWPFDocument doc;
+    private XWPFParagraph para;
+    XWPFRun run;
+    Path in;
+    Path out;
+
+    @BeforeEach
+    void setup() {
+        doc = new XWPFDocument();
+        para = doc.createParagraph();
+        run = para.createRun();
+        in = Paths.get("src/test/resources/inputTest.docx");
+        out = Paths.get("src/test/resources/outputTest.docx");
+    }
+
+    @Test
+    void writeDocumentTest() {
+        assertTrue(maker.writeDocument(doc, out));
+    }
+
+    @Test
+    void testIOExceptionInWriteDocument() throws IOException {
+        doc.close();
+        assertFalse(maker.writeDocument(doc, out));
+    }
+}
