e4141e5baa6d975f1386a1f02b50d55b13c71bea
cam-black <cameronablack1998@gmail.com>
2022-12-21 09:36:16 +0000

    Changed code to call convertWordToPDF method to check correct output achieved.

--- src/main/java/com/virginmoney/uk/Runner.java
+++ src/main/java/com/virginmoney/uk/Runner.java
@@ -2,12 +2,15 @@ package com.virginmoney.uk;
 
 import com.virginmoney.uk.entity.Person;
 import com.virginmoney.uk.service.PersonToFile;
+import com.virginmoney.uk.utils.converter.PDFConverter;
 
 import java.time.LocalDate;
 
 public class Runner {
-//    public static void main(String[] args) {
-//        PersonToFile toFile = new PersonToFile();
-//        toFile.replacePlaceholdersInText(new Person("John", "Doe", LocalDate.of(2000, 2, 12)));
-//    }
+    public static void main(String[] args) {
+        PersonToFile toFile = new PersonToFile();
+        Person john = new Person("John", "Doe", LocalDate.of(1982, 12, 13));
+        toFile.replacePlaceholdersInText(john);
+        PDFConverter.convertWordToPDF("output.docx", "output.pdf");
+    }
 }
