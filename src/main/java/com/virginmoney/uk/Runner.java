8f42b4c34c714320bde4f246fb8c8c43c8205690
cam-black <cameronablack1998@gmail.com>
2022-12-21 15:57:21 +0000

    Uncommented code.

--- src/main/java/com/virginmoney/uk/Runner.java
+++ src/main/java/com/virginmoney/uk/Runner.java
@@ -1,11 +1,20 @@
 package com.virginmoney.uk;
 
+import com.spire.doc.Document;
+import com.virginmoney.uk.entity.Person;
+import com.virginmoney.uk.service.PersonToFile;
+import com.virginmoney.uk.utils.converter.PDFConverter;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
 public class Runner {
-//    public static void main(String[] args) throws IOException {
-//        PersonToFile toFile = new PersonToFile();
-//        Person john = new Person("John", "Doe", LocalDate.of(1982, 12, 13));
-//        toFile.replacePlaceholdersInText(john);
-//        PDFConverter converter = new PDFConverter();
-//        converter.convertWordToPDF();
-//    }
+    public static void main(String[] args) throws IOException {
+        PersonToFile toFile = new PersonToFile();
+        Person john = new Person("John", "Doe", LocalDate.of(1982, 12, 13));
+        toFile.replacePlaceholdersInText(john);
+        Document doc = new Document("output.docx");
+        PDFConverter converter = new PDFConverter(doc);
+        converter.convertWordToPDF("output.pdf");
+    }
 }
