ed159b2103ea6077152e372d252d726a3f318866
cam-black <cameronablack1998@gmail.com>
2022-12-21 09:43:01 +0000

    Changed return signature of Method to boolean, enabling ability to test method easier.

--- src/main/java/com/virginmoney/uk/utils/converter/PDFConverter.java
+++ src/main/java/com/virginmoney/uk/utils/converter/PDFConverter.java
@@ -8,10 +8,17 @@ import org.apache.logging.log4j.Logger;
 public class PDFConverter {
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public static void convertWordToPDF(String in, String out) {
-        Document doc = new Document(in);
-        LOGGER.info("Converting to PDF...");
-        doc.saveToFile(out, FileFormat.PDF);
-        LOGGER.info("Conversion complete.");
+    public static boolean convertWordToPDF(String in, String out) {
+        try {
+            Document doc = new Document(in);
+
+            LOGGER.info("Converting to PDF...");
+            doc.saveToFile(out, FileFormat.PDF);
+            LOGGER.info("Conversion complete.");
+            return true;
+        } catch (Exception e) {
+            LOGGER.info(e);
+        }
+        return false;
     }
 }
