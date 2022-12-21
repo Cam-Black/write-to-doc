9412dd5fa07091f0cf054d0dd046de166086e416
cam-black <cameronablack1998@gmail.com>
2022-12-21 14:54:49 +0000

    Changed Converter to use fields and changed method from being static to enable better testing of methods.

--- src/main/java/com/virginmoney/uk/utils/converter/PDFConverter.java
+++ src/main/java/com/virginmoney/uk/utils/converter/PDFConverter.java
@@ -5,20 +5,50 @@ import com.spire.doc.FileFormat;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class PDFConverter {
-    private static final Logger LOGGER = LogManager.getLogger();
+    private final Logger LOGGER = LogManager.getLogger();
+    private final Path DOC_IN;
+    private final Path PDF_OUT;
+    private final Document DOC;
 
-    public static boolean convertWordToPDF(String in, String out) {
-        File f = new File(out);
-        Document doc = new Document(in);
-        LOGGER.info("Converting to PDF...");
-        doc.saveToFile(out, FileFormat.PDF);
-        if (f.isFile()) {
+    public PDFConverter() throws IOException {
+        super();
+        DOC_IN = Paths.get("output.docx");
+        PDF_OUT = Paths.get("output.pdf");
+        DOC = new Document(Files.newInputStream(DOC_IN));
+    }
+
+    public PDFConverter(String inDoc, String outPDF, Document doc) {
+        super();
+        DOC_IN = Paths.get(inDoc);
+        PDF_OUT = Paths.get(outPDF);
+        DOC = doc;
+    }
+
+    public PDFConverter(String inDoc, String outPdf) throws IOException {
+        super();
+        DOC_IN = Paths.get(inDoc);
+        PDF_OUT = Paths.get(outPdf);
+        DOC = new Document(Files.newInputStream(DOC_IN));
+    }
+
+    public boolean convertWordToPDF() {
+        try (OutputStream out = Files.newOutputStream(Paths.get(PDF_OUT.toString()))) {
+            LOGGER.info("Converting to PDF...");
+            DOC.saveToFile(out, FileFormat.PDF);
             LOGGER.info("Conversion complete.");
-            return true;
+            if (Files.isRegularFile(PDF_OUT)) return true;
+            else throw new IOException();
+        } catch (IOException e) {
+            LOGGER.error(e);
+            LOGGER.error("Conversion failed. Please check file paths are correct and not empty and input is .docx and output is .pdf format.");
+            return false;
         }
-        return false;
     }
 }
