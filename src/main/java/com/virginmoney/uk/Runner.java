db7b382960ceda675566528c1ab6a3b9ed3e96f2
cam-black <cameronablack1998@gmail.com>
2022-12-21 14:53:00 +0000

    Commented out lines to confirm actual coverage percentage.

--- src/main/java/com/virginmoney/uk/Runner.java
+++ src/main/java/com/virginmoney/uk/Runner.java
@@ -1,16 +1,11 @@
 package com.virginmoney.uk;
 
-import com.virginmoney.uk.entity.Person;
-import com.virginmoney.uk.service.PersonToFile;
-import com.virginmoney.uk.utils.converter.PDFConverter;
-
-import java.time.LocalDate;
-
 public class Runner {
-    public static void main(String[] args) {
-        PersonToFile toFile = new PersonToFile();
-        Person john = new Person("John", "Doe", LocalDate.of(1982, 12, 13));
-        toFile.replacePlaceholdersInText(john);
-        PDFConverter.convertWordToPDF("output.docx", "output.pdf");
-    }
+//    public static void main(String[] args) throws IOException {
+//        PersonToFile toFile = new PersonToFile();
+//        Person john = new Person("John", "Doe", LocalDate.of(1982, 12, 13));
+//        toFile.replacePlaceholdersInText(john);
+//        PDFConverter converter = new PDFConverter();
+//        converter.convertWordToPDF();
+//    }
 }
