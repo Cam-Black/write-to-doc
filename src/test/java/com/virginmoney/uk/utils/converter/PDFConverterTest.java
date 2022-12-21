b7bc58a6fa3e091e691018b6d4e1cac9d0cfc577
cam-black <cameronablack1998@gmail.com>
2022-12-21 14:55:22 +0000

    Implemented passing test checking a PDF is created with the specified file name.

--- src/test/java/com/virginmoney/uk/utils/converter/PDFConverterTest.java
+++ src/test/java/com/virginmoney/uk/utils/converter/PDFConverterTest.java
@@ -1,10 +1,28 @@
 package com.virginmoney.uk.utils.converter;
 
+import com.spire.doc.Document;
+import com.spire.doc.FileFormat;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 public class PDFConverterTest {
+    PDFConverter converter;
+    String outPdf = "src/test/resources/outputTest.pdf";
+    String inDoc = "src/test/resources/inputTest.docx";
+
+    @AfterEach
+    void removeTestFiles() throws IOException {
+        Files.deleteIfExists(Paths.get(outPdf));
+    }
 
     @Test
     void testPDFCreated() {
