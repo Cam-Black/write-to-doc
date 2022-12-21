42f395be201767ca7433ee43f6528c84b91027a5
cam-black <cameronablack1998@gmail.com>
2022-12-21 15:21:25 +0000

    re-added no args constructor.

--- src/main/java/com/virginmoney/uk/utils/worddocument/WordDocumentWriter.java
+++ src/main/java/com/virginmoney/uk/utils/worddocument/WordDocumentWriter.java
@@ -15,6 +15,12 @@ public class WordDocumentWriter {
     private final Path OUT;
     private final WordDocumentMaker MAKER;
 
+    public WordDocumentWriter() {
+        super();
+        OUT = Paths.get("output.docx");
+        MAKER = new WordDocumentMaker();
+    }
+
     public WordDocumentWriter(String out, WordDocumentMaker maker) {
         OUT = Paths.get(out);
         MAKER = maker;
